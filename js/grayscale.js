8629b697663722f88810b387ad51b1bac7cf1b9f
Aleks Kivuls <akivuls@yahoo.com>
2016-02-14 21:29:05 -0800

    Comment unused portion of google maps in js

--- js/grayscale.js
+++ js/grayscale.js
@@ -22,7 +22,7 @@ $('.navbar-collapse ul li a').click(function() {
 
 // Google Maps Scripts
 // When the window has finished loading create our google map below
-google.maps.event.addDomListener(window, 'load', init);
+/*google.maps.event.addDomListener(window, 'load', init);
 
 function init() {
     // Basic options for a simple Google Map
@@ -167,3 +167,4 @@ function init() {
         icon: image
     });
 }
+*/
