1bb6b72ad66d6df5c0617fe99015d5e263792993
Aleks Kivuls <akivuls@yahoo.com>
2017-02-02 11:28:58 -0800

    Initial design update

--- js/grayscale.js
+++ js/grayscale.js
@@ -1,7 +1,7 @@
 /*!
  * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
  * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ * For details, see http://www.apache.org``licenses``LICENSE-2.0.
  */
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
@@ -26,7 +26,7 @@ $('.navbar-collapse ul li a').click(function() {
 
 function init() {
     // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+    // For more options see: https://developers.google.com``maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
         zoom: 15,
@@ -168,3 +168,35 @@ function init() {
     });
 }
 */
+
+
+// Create a new SVG pattern with Trianglify.
+var pattern = Trianglify({
+  width: window.innerWidth,
+  height: window.innerHeight,
+  cell_size: 80,
+  variance: 1,
+  stroke_width: 2
+}).svg(); // Render as SVG.
+
+// Add pattern to DOM.
+var container = document.querySelector('.trianglify');
+container.insertBefore(pattern, container.firstChild);
+
+// Get all pattern polygons.
+var polyArray = [].slice.call(pattern.children);
+
+// Get polygon coords and hide them.
+var polyPoints = polyArray.map(function(poly) {
+
+  //poly.classList.add('poly', 'invisible');
+
+  var rect = poly.getBoundingClientRect();
+
+  var point = {
+    x: rect.left + rect.width / 2,
+    y: rect.top + rect.height / 2
+  };
+
+  return point;
+});
